b74d70a4872ff8bffc16f9ebcc63dbaf2867a916
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-06-23 13:28:48 +0200

    Printed question and answer on screen using foreach loop

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<?php 
+<?php
     include 'database.php';
-    
-    foreach ($data as $key => $value){
-        echo $key . ': '. $value;
-    }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -14,9 +8,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Google FAQ</title>
 </head>
 <body>
+<?php 
     
+    foreach ($data as $key => $value){ ?>
+        <div class="question">
+            <h2><?= $key ?></h2>
+            <p><?= $value?></p>
+        </div>    
+    <?php }   
+
+
+?>
 </body>
 </html>
